40f5a9fde3e55e9ae9ac41a4d576a4bfe49a02a0
Nick <ntoukas.nick@gmail.com>
2023-01-16 16:38:07 -0500

    display first job

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { FaAngleDoubleRight } from 'react-icons/fa';
 
 const url = 'https://course-api.com/react-tabs-project';
@@ -27,7 +27,31 @@ function App() {
     );
   }
 
-  return <h2>jobs</h2>;
+  const { company, dates, duties, title } = jobs[value];
+
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>experience</h2>
+        <div className='underline'></div>
+        <div className='jobs-center'>
+          <article className='job-info'>
+            <h3>{title}</h3>
+            <h4>{company}</h4>
+            <p className='job-date'>{dates}</p>
+            {duties.map((duty, index) => {
+              return (
+                <div key={index} className='job-desc'>
+                  <FaAngleDoubleRight className='job-icon' />
+                  <p>{duty}</p>
+                </div>
+              );
+            })}
+          </article>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default App;
